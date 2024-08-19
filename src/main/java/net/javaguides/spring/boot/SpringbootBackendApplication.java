f1761affd951d4e5a2c508a59a16e557f585812f
Naveen Kuratti <naveenkuratti875@gmail.com>
2024-08-19 16:26:01 +0530

    Your commit message

---
+++ src/main/java/net/javaguides/spring/boot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
